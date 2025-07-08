b7fffe4406a3d893b53cb686d2dd2c0a629f9a55
Steve Ferreira Da Silva <steve.silva@gpssa.com.br>
2025-07-07 22:45:26 -0300

    Adicionando classe StringGenerator com métodos para gerar texto aleatório com limite definido.

---
+++ TestUtilities/Generators/StringGeneratorTest.cs
@@ -0,0 +1,89 @@
+﻿using Utilities.Generators;
+
+namespace TestUtilities.Generators;
+public class StringGeneratorTest
+{
+    #region GenerateRandomLetters
+    [Fact]
+    public void GenerateRandomLetters_WithoutLimit_ReturnsOneRandomChar()
+    {
+        var result = StringGenerator.GenerateRandomLetters();
+
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Length);
+    }
+
+    [Theory]
+    [InlineData(5)]
+    [InlineData(10)]
+    [InlineData(50)]
+    public void GenerateRandomLetters_WithLimit_ReturnsCorrectLength(int limit)
+    {
+        var result = StringGenerator.GenerateRandomLetters(limit);
+
+        Assert.NotNull(result);
+        Assert.Equal(limit, result.Length);
+    }
+
+    [Fact]
+    public void GenerateRandomLetters_ReturnsOnlyLetters()
+    {
+        const string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        var result = StringGenerator.GenerateRandomLetters(100);
+
+        Assert.All(result, c => Assert.Contains(c, validChars));
+    }
+
+    [Fact]
+    public void GenerateRandomLetters_MultipleCalls_ShouldReturnDifferentValues()
+    {
+        var result1 = StringGenerator.GenerateRandomLetters(10);
+        var result2 = StringGenerator.GenerateRandomLetters(10);
+
+        Assert.NotEqual(result1, result2);
+    }
+    #endregion
+
+    #region GenerateRandomLetters
+    [Fact]
+    public void GenerateRandomAphaNumber_WithoutLimit_ReturnsOneRandomChar()
+    {
+        var result = StringGenerator.GenerateRandomAphaNumber();
+
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Length);
+    }
+
+    [Theory]
+    [InlineData(5)]
+    [InlineData(10)]
+    [InlineData(50)]
+    public void GenerateRandomAphaNumber_WithLimit_ReturnsCorrectLength(int limit)
+    {
+        var result = StringGenerator.GenerateRandomAphaNumber(limit);
+
+        Assert.NotNull(result);
+        Assert.Equal(limit, result.Length);
+    }
+
+    [Fact]
+    public void GenerateRandomAphaNumber_ReturnsOnlyLetters()
+    {
+        const string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        var result = StringGenerator.GenerateRandomAphaNumber(100);
+
+        Assert.All(result, c => Assert.Contains(c, validChars));
+    }
+
+    [Fact]
+    public void GenerateRandomAphaNumber_MultipleCalls_ShouldReturnDifferentValues()
+    {
+        var result1 = StringGenerator.GenerateRandomAphaNumber(10);
+        var result2 = StringGenerator.GenerateRandomAphaNumber(110);
+
+        Assert.NotEqual(result1, result2);
+    }
+    #endregion
+}
---
+++ Utilities/Generators/StringGenerator.cs
@@ -0,0 +1,44 @@
+﻿namespace Utilities.Generators
+{
+    /// <summary>
+    /// Gerador de textos aleatórios
+    /// </summary>
+    public static class StringGenerator
+    {
+        private static readonly Random _random = new Random();
+
+        /// <summary>
+        /// Gera uma string aleatória apenas com letras (A-Z, a-z) de tamanho definido.
+        /// </summary>
+        /// <param name="limit">
+        /// Quantidade de caracteres retornados pelo método.
+        /// </param>
+        /// <returns>
+        /// Retorna um texto aleatório com tamanho definido pelo argumento limit.
+        /// </returns>
+        public static string GenerateRandomLetters(int limit = 1)
+        {
+            const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            return new string(Enumerable.Range(0, limit)
+                .Select(_ => letters[_random.Next(letters.Length)])
+                .ToArray());
+        }
+
+        /// <summary>
+        /// Gera uma string aleatória com letras e números (A-Z, a-z, 0-9) de tamanho definido.
+        /// </summary>
+        /// <param name="limit">
+        /// Quantidade de caracteres retornados pelo método.
+        /// </param>
+        /// <returns>
+        /// Retorna um texto aleatório com tamanho definido pelo argumento limit.
+        /// </returns>
+        public static string GenerateRandomAphaNumber(int limit = 1)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Range(0, limit)
+                .Select(_ => chars[_random.Next(chars.Length)])
+                .ToArray());
+        }
+    }
+}
